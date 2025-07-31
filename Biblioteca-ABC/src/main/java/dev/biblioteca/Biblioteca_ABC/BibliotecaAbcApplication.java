9c956aa00be7ffc15dd1db95ee6cef853408b899
Eduardo Henrique Fritsche <edu.fritsche@gmail.com>
2025-07-31 19:22:24 -0300

    Init Commit

---
+++ Biblioteca-ABC/src/main/java/dev/biblioteca/Biblioteca_ABC/BibliotecaAbcApplication.java
@@ -0,0 +1,13 @@
+package dev.biblioteca.Biblioteca_ABC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaAbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaAbcApplication.class, args);
+	}
+
+}
